0a39b8e535a7ada597d3b2448e709e11476b4e50
aditd5 <aditdhiantono@gmail.com>
2023-05-26 20:40:12 +0700

    Project BWA MOV

---
+++ app/src/main/java/com/aditd5/bwamov/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.aditd5.bwamov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_activity)
+    }
+}
---
+++ app/src/main/java/com/aditd5/bwamov/onboarding/OnBoardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.aditd5.bwamov.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aditd5.bwamov.R
+
+class OnBoardingTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
